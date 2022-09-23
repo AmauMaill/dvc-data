ade3d1f4d3e6e5356ee27d5fda47209d77c9fa19
AmauMaill <amaury.maill@gmail.com>
2022-09-23 17:50:03 +0200

    continue to automate README

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Data repository with dvc
-## Todo
-## Data
+## Setup and usage
+### Requirements
+* First you need to install Data Version Control (DVC).
+* Then you need the acess credentials to the AWS account (for s3).
+### Setup
+* git clone this repository
+* execute the command dvc pull
+### Usage
+* dvc list <GitHub URL> to list the available files
+* dvc get <GitHub URL> <FILE> to get a copy
+## Catalogue
+data/
 rent.csv
--- readme.bash
+++ readme.bash
@@ -3,9 +3,22 @@ FILE_NAME="README.md"
 generate() {
 	echo "# Data repository with dvc" > ${FILE_NAME}
 
-	echo "## Todo" >> ${FILE_NAME}
+	echo "## Setup and usage" >> ${FILE_NAME}
 
-	echo "## Data" >> ${FILE_NAME}
+	echo "### Requirements" >> ${FILE_NAME}
+	echo "* First you need to install Data Version Control (DVC)." >> ${FILE_NAME}
+	echo "* Then you need the acess credentials to the AWS account (for s3)." >> ${FILE_NAME}
+
+	echo "### Setup" >> ${FILE_NAME}
+	echo "* git clone this repository" >> ${FILE_NAME}
+	echo "* execute the command dvc pull" >> ${FILE_NAME}
+
+	echo "### Usage" >> ${FILE_NAME}
+	echo "* dvc list <GitHub URL> to list the available files" >> ${FILE_NAME}
+	echo "* dvc get <GitHub URL> <FILE> to get a copy" >> ${FILE_NAME}
+
+	echo "## Catalogue" >> ${FILE_NAME}
+	echo "data/" >> ${FILE_NAME}
 	ls data >> ${FILE_NAME}
 }
 
