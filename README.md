525aa741e9a755117e90895351a09c58cdbccc51
AmauMaill <amaury.maill@gmail.com>
2022-09-23 17:59:23 +0200

    Bash README

--- README.md
+++ README.md
@@ -3,12 +3,9 @@
 ### Requirements
 * First you need to install Data Version Control (DVC).
 * Then you need the acess credentials to the AWS account (for s3).
-### Setup
-* `git clone` this repository
-* execute the command `dvc pull`
 ### Usage
 * `dvc list <GitHub URL>` to list the available files
-* `dvc get <GitHub URL> <FILE>` to get a copy
+* `dvc get <GitHub URL> <DIRECTORY/FILE>` to get a copy
 ## Catalogue
 `data/`
 
--- readme.bash
+++ readme.bash
@@ -9,13 +9,9 @@ generate() {
 	echo "* First you need to install Data Version Control (DVC)." >> ${FILE_NAME}
 	echo "* Then you need the acess credentials to the AWS account (for s3)." >> ${FILE_NAME}
 
-	echo "### Setup" >> ${FILE_NAME}
-	echo '* `git clone` this repository' >> ${FILE_NAME}
-	echo '* execute the command `dvc pull`' >> ${FILE_NAME}
-
 	echo "### Usage" >> ${FILE_NAME}
 	echo '* `dvc list <GitHub URL>` to list the available files' >> ${FILE_NAME}
-	echo '* `dvc get <GitHub URL> <FILE>` to get a copy' >> ${FILE_NAME}
+	echo '* `dvc get <GitHub URL> <DIRECTORY/FILE>` to get a copy' >> ${FILE_NAME}
 
 	echo "## Catalogue" >> ${FILE_NAME}
 	echo '`data/`' >> ${FILE_NAME}
